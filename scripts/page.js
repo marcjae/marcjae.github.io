13fdb692cea3fd1adc7d88f27b5c8b0e074d639b
Marc Canham <marcjae@gmail.com>
2014-04-10 10:40:23 +0200

    Page holder

--- scripts/page.js
+++ scripts/page.js
@@ -1,6 +1,52 @@
 // @koala-prepend "jquery.js"
+// @koala-prepend "libs.js"
 
-// jQuery is ready
+app = {}
+app.sections = function(action){
+	
+	var section = $('.section')
+	
+	// Update
+	function update_heights() {
+		
+		//if ( $(window).height() > section.height() ) {
+			section.height($(window).height()-20)
+			$('.content.centered').height(section.height())
+			$('.section.home').waypoint({
+			  handler: function(direction) {
+			  		if ( direction === 'down' ) {
+			  			$('#header').addClass('fixed')
+			  		} else {
+			  			$('#header').removeClass('fixed')
+			  		}
+			  },
+			  offset: -$('.section.home').height()
+			});			 
+		//}		
+	}
+	
+	if ( action === 'adjust' ) {
+		update_heights();
+	}	
+}
+app.init = function(){
+	this.sections('adjust')
+}
+
+
 $(document).ready(function(){
-	// Doc ready
+	// Document ready
+	
+
+
+
+
+	
+	app.init();
+	
+})
+
+
+$(window).resize(function(){
+	app.sections('adjust')
 })
